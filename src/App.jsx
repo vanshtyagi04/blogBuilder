23af38bcd02a77b167afc670a56c367474826ff4
vanshtyagi04 <vanshtyagi4526@gmail.com>
2024-10-13 10:57:48 +0530

    initial setup of App.jsx with loading feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,50 @@
+import React, { useState, useEffect}from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login , logout } from './store/features/authSlice'
+import {Header , Footer} from './components/index.js'
 
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
 
-  return (
-    
-    <>
-    
-    </>
-  )
+  useEffect(() =>{
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login(userData));
+      }
+      else{
+        dispatch(logout());
+      }
+    })
+    .catch((error) => {
+      console.log("Error :: App :: useEffect :: authService.getCurrentUser");
+      console.log(error)
+    })
+    .finally(() => {
+      setLoading(false);
+    })
+  } , [])
+
+  if(loading){
+    return(
+      <div>Loading</div>
+    )
+  }
+  else{
+    return(
+      <div className = "min-h-screen flex flex-wrap content-between bg-blue-600">
+        <div className= 'w-full block'>
+          <Header/>
+          {/* <Outlet/> */}
+          <Footer/>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+
+
+export{
+    Header,
+    Footer,
+}
+
+
