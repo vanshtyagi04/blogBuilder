1fbc231dca470957b9cb09a31b591b220d1e320c
vanshtyagi04 <vanshtyagi4526@gmail.com>
2024-10-13 13:15:52 +0530

    components designed : header , container , footer , logout button , reusable buttton , reusable input feild

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,19 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
-
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import Container from "./container/Container";
+import Input from "./reusableComp/Input";
+import Button from "./reusableComp/Button";
 
 export{
     Header,
     Footer,
+    Container,
+    LogoutBtn,
+    Logo,
+    Input,
+    Button,
 }
 
 
